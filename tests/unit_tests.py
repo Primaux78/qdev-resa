2238412ae4380259aaef21f131bff64cef4d9948
Primaux78 <brice.romanet@gmail.com>
2026-03-13 11:50:35 +0100

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,8 @@ from resa.resa import *
 class bookMeetingRoomUnitTests(unittest.TestCase):
   def test_small_room(self):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
+    self.assertEqual(bookMeetingRoom(15), Room.MEDIUM)
+    
 
 if __name__ == '__main__':
   unittest.main()
