2c43578c2efdbe0bb55171ff1039b9bf9a15a206
Primaux78 <brice.romanet@gmail.com>
2026-03-13 11:51:50 +0100

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,6 +5,8 @@ class bookMeetingRoomUnitTests(unittest.TestCase):
   def test_small_room(self):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
     self.assertEqual(bookMeetingRoom(15), Room.MEDIUM)
+    self.assertEqual(bookMeetingRoom(40), Room.LARGE)
+    self.assertEqual(bookMeetingRoom(67), Room.REFUSE)
     
 
 if __name__ == '__main__':
